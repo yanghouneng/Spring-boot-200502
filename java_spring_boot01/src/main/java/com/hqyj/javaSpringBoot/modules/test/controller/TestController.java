e3a608dc00b508fdef0dd089959b81d4d5242f8b
yanghouneng <1778554829@qq.com>
2020-08-10 22:35:53 +0800

    2020.08.10  代码测试：配置文件的读取以及logback日志测试

--- java_spring_boot01/src/main/java/com/hqyj/javaSpringBoot/modules/test/controller/TestController.java
+++ java_spring_boot01/src/main/java/com/hqyj/javaSpringBoot/modules/test/controller/TestController.java
@@ -1,17 +1,78 @@
 package com.hqyj.javaSpringBoot.modules.test.controller;
 
+import com.hqyj.javaSpringBoot.modules.test.vo.ApplicationTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("test")
+@RequestMapping("/test")
 public class TestController {
 
-    @RequestMapping("txt")
+    private final static Logger LOGGER = LoggerFactory.getLogger(TestController.class);
+
+    @Autowired
+    private ApplicationTest applicationTest;
+
+    @Value("${server.port}")
+    private String port;
+
+    @Value("${com.name}")
+    private String name;
+
+    @Value("${com.age}")
+    private int age;
+
+    @Value("${com.desc}")
+    private String desc;
+
+    @Value("${com.random}")
+    private String random;
+
+    /*
+    * 127.0.0.1:8085/test/log
+    * */
+    @GetMapping("log")
+    @ResponseBody
+    public String logTest(){
+        LOGGER.trace("This is trace log");
+        LOGGER.debug("This is debug log");
+        LOGGER.info("This is info log");
+        LOGGER.warn("This is warn log");
+        LOGGER.error("This is error log");
+        return "This is log test";
+    }
+
+    /*
+    * 127.0.0.1:8085/test/config
+    * */
+    @GetMapping("config")
+    @ResponseBody
+    public String configTest(){
+        StringBuffer stringBuffer = new StringBuffer();
+                stringBuffer.append(port).append("----")
+                .append(name).append("----")
+                .append(age).append("----")
+                .append(desc).append("----")
+                .append(random).append("---").append("<br>");
+
+                stringBuffer.append(applicationTest.getPort()).append("-----")
+                        .append(applicationTest.getName()).append("------")
+                        .append(applicationTest.getAge()).append("-----")
+                        .append(applicationTest.getDesc()).append("------")
+                        .append(applicationTest.getRandom()).append("----").append("<br>");
+                return stringBuffer.toString();
+    }
+
+    @RequestMapping("/txt")
     @ResponseBody
     public String text(){
 
-        return "这是一个字符串";
+        return "HelloWorld";
     }
 }
---
+++ java_spring_boot01/src/main/java/com/hqyj/javaSpringBoot/modules/test/vo/ApplicationTest.java
@@ -0,0 +1,57 @@
+package com.hqyj.javaSpringBoot.modules.test.vo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource("classpath:config/applicationTest.properties")
+@ConfigurationProperties("com.qq")
+public class ApplicationTest {
+    private String port;
+    private String name;
+    private int age;
+    private String desc;
+    private String random;
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getRandom() {
+        return random;
+    }
+
+    public void setRandom(String random) {
+        this.random = random;
+    }
+}
