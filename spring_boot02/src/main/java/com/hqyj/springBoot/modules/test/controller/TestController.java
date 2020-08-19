8f6c8b9c437384c0f5be424b0901c9275481a006
yanghouneng <1778554829@qq.com>
2020-08-19 21:44:31 +0800

    2020.08.18-19 作业：文件上传下载  监控  拆解前端模板

--- spring_boot02/src/main/java/com/hqyj/springBoot/modules/test/controller/TestController.java
+++ spring_boot02/src/main/java/com/hqyj/springBoot/modules/test/controller/TestController.java
@@ -9,14 +9,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -33,6 +41,153 @@ public class TestController {
     private CityService cityService;
     @Autowired
     private CountryServcie countryServcie;
+
+    /*
+    * 127.0.0.1/test/indexPage
+    * */
+    @GetMapping("/indexSimple")
+    public String indexPageTest(){
+        return "indexSimple";
+    }
+
+    /*
+    * 127.0.0.1/test/down
+    * */
+    //下载文件
+    @GetMapping("/down")
+    public ResponseEntity<Resource> fileDownLoad(String fileName){
+        Resource resource = null;
+        try {
+            resource = new UrlResource(
+                    Paths.get("D:\\upload\\" + fileName).toUri());//获取文件下找的path路径
+            if (resource.exists()&&resource.isReadable()){
+                //HttpHeaders.CONTENT_TYPE包装了content-type，  HttpHeaders.CONTENT_DISPOSITION对下载的一个描述
+                return ResponseEntity.ok().header(HttpHeaders.CONTENT_TYPE,"application/octet-stream")
+                        .header(HttpHeaders.CONTENT_DISPOSITION,
+                                String.format("attachment; filename=\"%s\"", resource.getFilename()))
+                        .body(resource);//body()//传输给页面的文件内容    filename="%s"下载内容的名称
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 将文件以BufferedInputStream的方式读取到byte[]里面，然后用OutputStream.write输出文件
+     */
+    @RequestMapping("/download1")
+    public void downloadFile1(HttpServletRequest request,
+                              HttpServletResponse response, @RequestParam String fileName) {
+        String filePath = "D:/upload" + File.separator + fileName;
+        File downloadFile = new File(filePath);
+
+        if (downloadFile.exists()) {
+            response.setContentType("application/octet-stream");
+            response.setContentLength((int)downloadFile.length());
+            response.setHeader(HttpHeaders.CONTENT_DISPOSITION,
+                    String.format("attachment; filename=\"%s\"", fileName));
+
+            byte[] buffer = new byte[1024];
+            FileInputStream fis = null;
+            BufferedInputStream bis = null;
+            try {
+                fis = new FileInputStream(downloadFile);
+                bis = new BufferedInputStream(fis);
+                OutputStream os = response.getOutputStream();
+                int i = bis.read(buffer);
+                while (i != -1) {
+                    os.write(buffer, 0, i);
+                    i = bis.read(buffer);
+                }
+            } catch (Exception e) {
+                LOGGER.debug(e.getMessage());
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (fis != null) {
+                        fis.close();
+                    }
+                    if (bis != null) {
+                        bis.close();
+                    }
+                } catch (Exception e2) {
+                    LOGGER.debug(e2.getMessage());
+                    e2.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /*
+    * 127.0.0.1/test/files
+    * */
+    //上传多个文件
+    @PostMapping(value = "/files",consumes = "multipart/form-data")
+    public String filesUpload(@RequestParam MultipartFile[] files, RedirectAttributes redirectAttributes) {
+        boolean empty = true;//定义是否为真
+        try {
+            for (MultipartFile file : files) { //遍历
+                if (file.isEmpty()) {   //如果上传的文件为空
+                    continue;
+                }
+
+                String destFilePath = "D:\\upload\\" + file.getOriginalFilename();
+                File destFile = new File(destFilePath);
+                file.transferTo(destFile); //转存到目标文件中
+                empty = false;
+            }
+
+            if (empty) {
+                redirectAttributes.addFlashAttribute(
+                        "messages", "上传失败");
+            } else {
+                redirectAttributes.addFlashAttribute(
+                        "messages", "上传成功");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            redirectAttributes.addFlashAttribute(
+                    "messages", "上传失败");
+        }
+
+        return "redirect:/test/index";
+    }
+
+    /*
+    * 127.0.0.1/test/file
+    * */
+    //单个文件上传
+    @PostMapping(value = "/file",consumes = "multipart/form-data")
+    public String fileUpload(@RequestParam MultipartFile file, RedirectAttributes redirectAttributes){
+        //如果上传的文件为null
+        if (file.isEmpty()){
+            redirectAttributes.addFlashAttribute("message","上传失败");
+            return "redirect:/test/index";
+        }
+        try {
+            //定义文件所在的位置
+            String destPath = "D:\\upload\\" + file.getOriginalFilename();
+            System.err.println(destPath);
+            File file1 = new File(destPath);
+            file.transferTo(file1);  //转存到目标文件中
+
+            // 使用工具类Files来上传文件
+//			byte[] bytes = file.getBytes();
+//			Path path = Paths.get(destFileName);
+//			Files.write(path, bytes);
+
+            redirectAttributes.addFlashAttribute("message","上传成功");
+
+        }catch (IOException e){
+            e.printStackTrace();
+            redirectAttributes.addFlashAttribute("message","上传失败");
+        }
+        return "redirect:/test/index";
+    }
+
+
 
     private final static Logger LOGGER = LoggerFactory.getLogger(TestController.class);
 
@@ -72,7 +227,7 @@ public class TestController {
                "/upload/111.png");
        modelMap.addAttribute("country", country);
        modelMap.addAttribute("cities", cities);
-       modelMap.addAttribute("updateCityUri", "/api/city");
+       modelMap.addAttribute("updateCityUri", "/api/updateCity");
        modelMap.addAttribute("template", "test/index");
        //返回外部碎片的组装器
         return "index";
